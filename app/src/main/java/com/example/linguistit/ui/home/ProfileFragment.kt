8c88cfa3c574a1091a6898055cdbcd705d268de7
Mario Jasso <jasso.munoz.mario.alberto@gmail.com>
2026-05-13 21:12:24 -0600

    feat : "Se ha implementado el Home junto con su navegación"

---
+++ app/src/main/java/com/example/linguistit/ui/home/ProfileFragment.kt
@@ -0,0 +1,6 @@
+package com.example.linguistit.ui.home
+
+import androidx.fragment.app.Fragment
+import com.example.linguistit.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile)
---
+++ app/src/main/java/com/example/linguistit/ui/home/ProjectsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.linguistit.ui.home
+
+import androidx.fragment.app.Fragment
+import com.example.linguistit.R
+
+class ProjectsFragment : Fragment(R.layout.fragment_projects)
